4fda64e78103a06389686d9fabcf57f4e81e5653
Khánh Hoàng <6380436+hckhanh@users.noreply.github.com>
2023-12-17 10:35:29 +0700

    update layout of OrderDetails route

--- app/app/routes/_store.orders.$orderId/PaymentInformation.tsx
+++ app/app/routes/_store.orders.$orderId/PaymentInformation.tsx
@@ -1,39 +1,45 @@
-import TextInput from '~/components/form/TextInput'
 import FormSection from '~/components/form/FormSection'
+import TextInput from '~/components/form/TextInput'
 
 export default function PaymentInformation() {
   return (
-    <div className='flex flex-col gap-12'>
-      <FormSection title='Payment details'>
-        <TextInput
-          className='col-span-full'
-          type='text'
-          autoComplete='cc-name'
-          label='Name on card'
-        />
-        <TextInput
-          className='col-span-full'
-          type='text'
-          autoComplete='cc-number'
-          label='Card number'
-        />
-        <TextInput
-          className='col-span-2'
-          type='text'
-          autoComplete='cc-exp'
-          placeholder='01/25'
-          pattern='^(0[1-9]|1[0-2])\/\d{2}$'
-          label='Expiration date (MM/YY)'
-        />
-        <TextInput
-          className='col-span-1'
-          type='password'
-          autoComplete='cc-csc'
-          label='CVC'
-          maxLength={3}
-          size={3}
-        />
-      </FormSection>
-    </div>
+    <FormSection title='Payment details'>
+      <TextInput
+        required
+        type='text'
+        name='nameOnCard'
+        label='Name on card'
+        autoComplete='cc-name'
+        className='col-span-full'
+      />
+      <TextInput
+        required
+        type='text'
+        name='cardNumber'
+        label='Card number'
+        autoComplete='cc-number'
+        className='col-span-full'
+      />
+      <TextInput
+        required
+        type='text'
+        placeholder='01/25'
+        autoComplete='cc-exp'
+        name='cardExpiration'
+        className='col-span-3'
+        label='Expiration date (MM/YY)'
+        pattern='^(0[1-9]|1[0-2])\/\d{2}$'
+      />
+      <TextInput
+        required
+        size={3}
+        label='CVC'
+        maxLength={3}
+        name='cardCvc'
+        type='password'
+        autoComplete='cc-csc'
+        className='col-span-1'
+      />
+    </FormSection>
   )
 }
--- app/app/routes/_store.orders.$orderId/route.tsx
+++ app/app/routes/_store.orders.$orderId/route.tsx
@@ -1,19 +1,32 @@
-import PaymentInformation from '~/routes/_store.orders.$orderId/PaymentInformation'
-import OrderPayNow from '~/routes/_store.orders.$orderId/OrderPayNow'
+import { type LoaderFunctionArgs, json } from '@remix-run/node'
+import { Form, useLoaderData } from '@remix-run/react'
 import Breadcrumb from '~/components/Breadcrumb'
-import { Form } from '@remix-run/react'
+import prisma from '~/prisma'
+import OrderPayNow from '~/routes/_store.orders.$orderId/OrderPayNow'
+import PaymentInformation from '~/routes/_store.orders.$orderId/PaymentInformation'
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const order = await prisma.order.findUniqueOrThrow({
+    where: { id: params.orderId },
+  })
+
+  return json(order)
+}
 
 export default function OrderDetails() {
+  const order = useLoaderData<typeof loader>()
+
   return (
-    <Form className='grid grid-cols-1 gap-8 lg:grid-cols-2'>
-      <Breadcrumb className='col-span-full col-start-2 2xl:col-start-1'>
+    <Form className='gap-8 lg:grid lg:grid-cols-12'>
+      <Breadcrumb className='col-span-full col-start-2 mb-8 lg:mb-0 2xl:col-start-1'>
         <Breadcrumb.Item link='/'>Home</Breadcrumb.Item>
-        <Breadcrumb.Item link='..'>Nomad Tumbler</Breadcrumb.Item>
-        <Breadcrumb.ItemCurrent>Order</Breadcrumb.ItemCurrent>
+        <Breadcrumb.ItemCurrent>Order {order.id}</Breadcrumb.ItemCurrent>
       </Breadcrumb>
 
-      <PaymentInformation />
-      <div className='space-y-6'>
+      <div className='col-span-5 col-start-2 row-span-2 2xl:col-span-7 2xl:col-start-1'>
+        <PaymentInformation />
+      </div>
+      <div className='col-span-5 row-span-2 space-y-6'>
         <h2 className='text-lg font-medium leading-7'>Order summary</h2>
         <OrderPayNow />
       </div>
