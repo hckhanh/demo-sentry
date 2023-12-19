eb7b4e8dc932f8588cddebba2d36496669df972d
Khánh Hoàng <6380436+hckhanh@users.noreply.github.com>
2023-12-19 09:11:32 +0700

    add email-worker

--- components/workers/email-worker/index.ts
+++ components/workers/email-worker/index.ts
@@ -1,8 +1,26 @@
-import {serve} from 'bun'
+import { Worker } from 'bullmq'
 
-serve({
-  development: true,
-  fetch(req) {
-    throw new Error("woops!");
-  },
-});
+const worker = new Worker('myQueue', async job => {
+  // Processing of your job here...
+  console.log('Processing job with id: ', job.id)
+}, {
+  concurrency:1,
+  connection: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+  }
+})
+
+worker.on("error", (error) => {
+  console.log("Error " + error)
+})
+
+worker.on("failed", (job, error) => {
+  console.log("Failed " + error)
+})
+
+worker.on("completed", (job) => {
+  console.log("Completed " + job.id)
+})
