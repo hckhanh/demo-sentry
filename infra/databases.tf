cb3ddb45f7d34baab96145219fc458d61fe8add2
Khánh Hoàng <6380436+hckhanh@users.noreply.github.com>
2023-12-24 18:46:49 +0700

    Remove digitalocean_database_db resource from Terraform scripts
    This commit removes the 'digitalocean_database_db' resource from our Terraform infrastructure scripts. We've instead started directly referencing the 'digitalocean_database_cluster' ID for all other dependent resources, eliminating the need for the now-removed database resource.

--- infra/databases.tf
+++ infra/databases.tf
@@ -10,13 +10,8 @@ resource "digitalocean_database_cluster" "demo_sentry_db" {
   project_id           = digitalocean_project.demo_sentry.id
 }
 
-resource "digitalocean_database_db" "demo_sentry_db" {
-  cluster_id = digitalocean_database_cluster.demo_sentry_db.id
-  name       = "demo-sentry-db"
-}
-
 resource "digitalocean_database_firewall" "demo_sentry_db" {
-  cluster_id = digitalocean_database_db.demo_sentry_db.cluster_id
+  cluster_id = digitalocean_database_cluster.demo_sentry_db.id
 
   rule {
     type  = "app"
@@ -25,15 +20,15 @@ resource "digitalocean_database_firewall" "demo_sentry_db" {
 }
 
 resource "digitalocean_database_connection_pool" "demo_sentry_db" {
-  cluster_id = digitalocean_database_db.demo_sentry_db.cluster_id
+  cluster_id = digitalocean_database_cluster.demo_sentry_db.id
   name       = "demo-sentry-db-pool"
   mode       = "transaction"
   size       = 5
-  db_name    = digitalocean_database_db.demo_sentry_db.name
+  db_name    = digitalocean_database_cluster.demo_sentry_db.database
 }
 
 resource "digitalocean_database_replica" "demo_sentry_db_1" {
-  cluster_id = digitalocean_database_db.demo_sentry_db.cluster_id
+  cluster_id = digitalocean_database_cluster.demo_sentry_db.id
   name       = "demo-sentry-db-replica-1"
   size       = "db-s-1vcpu-1gb"
   region     = "sgp1"
@@ -51,7 +46,7 @@ resource "digitalocean_database_firewall" "demo_sentry_db_1" {
 }
 
 resource "digitalocean_database_replica" "demo_sentry_db_2" {
-  cluster_id = digitalocean_database_db.demo_sentry_db.cluster_id
+  cluster_id = digitalocean_database_cluster.demo_sentry_db.id
   name       = "demo-sentry-db-replica-2"
   size       = "db-s-1vcpu-1gb"
   region     = "sgp1"
